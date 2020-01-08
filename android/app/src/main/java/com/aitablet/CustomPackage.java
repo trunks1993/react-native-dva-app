a7eabc3a9476c8f1ba2f67afd291f56dfd937154
trunks1993 <604723443@qq.com>
2020-01-08 11:20:39 +0800

    clear run error code

--- android/app/src/main/java/com/aitablet/CustomPackage.java
+++ android/app/src/main/java/com/aitablet/CustomPackage.java
@@ -2,10 +2,7 @@ package com.aitablet;
 
 import androidx.annotation.NonNull;
 
-import com.aitablet.clock.AlarmModle;
-import com.aitablet.clock.PlayMusicModle;
-import com.aitablet.fingerprint.FingerModle;
-import com.aitablet.readcard.ReadCardModle;
+import com.aitablet.fingerprint.FingerModule;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -20,10 +17,7 @@ public class CustomPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new FingerModle(reactContext));
-        modules.add(new ReadCardModle(reactContext));
-        modules.add(new AlarmModle(reactContext));
-        modules.add(new PlayMusicModle(reactContext));
+        modules.add(new FingerModule(reactContext));
         return modules;
     }
 
